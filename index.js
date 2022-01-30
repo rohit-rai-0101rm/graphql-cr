4596f911af9680012dca55feb19f3030d4874f9a
rohit rai <rohitrai.dev@gmail.com>
2022-01-30 10:50:16 +0530

    graphql create/read

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Start your es6 scripts here
+import express from 'express'
+import resolvers from './resolvers'
+import schema from './schema'
+import{graphqlHTTP} from 'express-graphql'
+const app=express()
+app.get("/",(req,res)=>{
+    res.send("up and running")
+
+})
+const root=resolvers
+app.use('/graphql',graphqlHTTP({
+    schema:schema,
+    rootValue:root,
+    graphiql:true
+}))
+app.listen(8000,()=>console.log("running on 8000"))
